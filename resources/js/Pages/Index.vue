9097d9446a129aeca950eb2883aaca3180c00b59
Lambertn33 <lambertgithub@gmail.com>
2024-02-14 08:51:36 +0200

    feat: setup vue and TS

---
+++ resources/js/Pages/Index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h2>Hello world</h2>
+    </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{message: string}>()
+
+console.log(props.message);
+</script>
