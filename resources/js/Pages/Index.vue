9e5a135547eb72ea7053891e323c47c3b431dbbd
Lambertn33 <lambertgithub@gmail.com>
2024-02-29 14:36:53 +0200

    feat: add home page

--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -1,7 +1,49 @@
 <template>
-    <div>
-        <h2>Hello world</h2>
-    </div>
+    <section className="bg-black dark:bg-gray-900">
+        <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
+            <div className="max-w-screen-lg">
+                <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-white">
+                    Search any artist or album within seconds..
+                </h2>
+                <p className="mb-8 font-light text-white text-sm">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos
+                    praesentium a inventore repellat nisi sunt ad! Ratione sequi
+                    assumenda recusandae eos cum hic sunt, eligendi at veniam voluptas
+                    voluptatum maiores! Lorem ipsum dolor sit amet consectetur,
+                    adipisicing elit. Maiores, veniam hic. Quo, nostrum facere eius
+                    reiciendis voluptates veritatis beatae dolor iusto reprehenderit
+                    adipisci architecto quae ad sit esse aut exercitationem.
+                </p>
+            </div>
+        </div>
+    </section>
+    <section className="bg-[#0e7470] rounded-lg mb-16">
+        <div className="gap-16 items-center px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
+            <div className="font-light text-white sm:text-lg dark:text-gray-400">
+                <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+                    Ready to start?
+                </h2>
+                <p className="mb-4 text-sm text-justify">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
+                    blanditiis laborum, libero fuga ut ullam error debitis quam hic
+                    animi veniam ratione dignissimos sint voluptatibus ex quia eius
+                    assumenda totam! Lorem ipsum dolor sit amet consectetur
+                    adipisicing elit. Cumque consectetur quo corrupti exercitationem
+                    ipsam sit quod rerum tenetur dolore necessitatibus, eius autem
+                    quas asperiores magni dolorem repellendus ipsum voluptatibus
+                    fugiat.
+                </p>
+            </div>
+            <div className="grid grid-cols-2 gap-4 mt-8">
+                <img className="w-full rounded-lg"
+                    src="https://images.unsplash.com/photo-1484876065684-b683cf17d276?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    alt="image 1" />
+                <img className="mt-4 w-full lg:mt-10 rounded-lg"
+                    src="https://images.unsplash.com/photo-1525362081669-2b476bb628c3?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    alt="image 2" />
+            </div>
+        </div>
+    </section>
 </template>
 
 <script lang="ts" setup>
