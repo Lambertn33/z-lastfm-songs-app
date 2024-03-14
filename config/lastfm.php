e00a694d6e19ecbb412cbc3cd3affdc142e400b0
Lambertn33 <lambertgithub@gmail.com>
2024-03-14 23:02:48 +0200

    feat: add last fm variables in caches

--- config/lastfm.php
+++ config/lastfm.php
@@ -6,5 +6,6 @@ return [
      * You can get your own last.fm API key at http://www.last.fm/api/account/create.
      */
     'api_key' => env('LASTFM_API_KEY'),
+    'api_url' => env('LASTFM_API_URL')
 
 ];
