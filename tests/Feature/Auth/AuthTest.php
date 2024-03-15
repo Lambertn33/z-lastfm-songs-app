3520c6ec4c889fb3d7b2d24d69823dda6990696a
Lambertn33 <lambertgithub@gmail.com>
2024-03-15 14:03:57 +0200

    feat: add auth tests

--- tests/Feature/Auth/AuthTest.php
+++ tests/Feature/Auth/AuthTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Auth;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -10,9 +11,19 @@ use Laravel\Socialite\Facades\Socialite;
 use Laravel\Socialite\Two\AbstractProvider;
 use Mockery;
 use Laravel\Socialite\Two\User as SocialiteUser;
+use Mockery\MockInterface;
 
 class AuthTest extends TestCase
 {
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Mock Google client ID and client secret for testing
+        config(['services.google.client_id' => 'mock_client_id']);
+        config(['services.google.client_secret' => 'mock_client_secret']);
+    }
+
     public function test_index_method_returns_view()
     {
         $this->get('/auth')->assertInertia(fn (Assert $page) => $page->component('Auth'));
@@ -27,5 +38,19 @@ class AuthTest extends TestCase
         $redirectUrl = $response->headers->get('Location');
 
         $this->assertStringStartsWith('https://accounts.google.com/o/oauth2', $redirectUrl);
+    }
+
+
+    public function test_logout_method()
+    {
+        $user = User::factory()->create();
+
+        $this->actingAs($user);
+
+        $response = $this->post('/auth/logout');
+
+        $this->assertGuest();
+
+        $response->assertRedirect(route('auth.index'));
     }
 }
