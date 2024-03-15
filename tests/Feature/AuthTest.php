8448a1215173dda0bde94237ca42f254fe243991
Lambertn33 <lambertgithub@gmail.com>
2024-03-15 07:57:21 +0200

    feat: test artist searvices

---
+++ tests/Feature/AuthTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use Inertia\Testing\AssertableInertia as Assert;
+use Laravel\Socialite\Facades\Socialite;
+use Laravel\Socialite\Two\AbstractProvider;
+use Mockery;
+use Laravel\Socialite\Two\User as SocialiteUser;
+
+class AuthTest extends TestCase
+{
+    public function test_index_method_returns_view()
+    {
+        $this->get('/auth')->assertInertia(fn (Assert $page) => $page->component('Auth'));
+    }
+
+    public function test_redirect_method_redirects_to_google_auth()
+    {
+        $response = $this->get('/auth/redirect');
+
+        $response->assertRedirect();
+
+        $redirectUrl = $response->headers->get('Location');
+
+        $this->assertStringStartsWith('https://accounts.google.com/o/oauth2', $redirectUrl);
+    }
+}
